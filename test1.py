4de517678f882c0f977a65c7cf2a1193a184739c
unknown <shahsarthak853@gmail.com>
2026-03-16 12:30:18 +0530

    Initial Commit

--- test1.py
+++ test1.py
@@ -20,7 +20,7 @@ plt.show() #Shows that netflix contains more movies
 #Content Released per year
 release_year = df['release_year'].value_counts().sort_index()
 plt.figure(figsize=(10,5))
-release_year.plot()
+sns.lineplot(x=release_year.index, y=release_year.values, marker="o")
 plt.title("Content released per year")
 plt.xlabel("Year")
 plt.ylabel("Number of shows")
@@ -29,8 +29,13 @@ plt.show() # Shows that content released per year increased after 2015
 #Shows Top 10 Countries Producing Content
 top_countries = df['country'].value_counts().head(10)
 plt.figure(figsize=(10,5))
-top_countries.plot(kind="bar")
+sns.barplot(
+    x=top_countries.index,
+    y=top_countries.values
+)
 plt.title("Top 10 Content Producing Countries")
+plt.xlabel("Country")
+plt.ylabel("Number of Titles")
 plt.savefig('top_10_content.jpg')
 plt.show() #USA Leads the World
 #Years In Which Most Content was added 
@@ -47,7 +52,9 @@ print(avg_duration)
 genres = df['listed_in'].str.split(" ", expand=True).stack()
 top_genres = genres.value_counts().head(10)
 plt.figure(figsize=(10,5))
-top_genres.plot(kind='bar')
+sns.barplot(x=top_genres.index, y=top_genres.values)
 plt.title("Top Genres on Netflix")
+plt.xlabel("Genres")
+plt.ylabel("Number of Genres")
 plt.savefig('genres.jpg')
 plt.show()
